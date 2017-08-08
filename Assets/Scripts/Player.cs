1bb6e50d62cf593dc32e2e8563215eabb117a0fd
AMeghnad <anshumanmeghnad@gmail.com>
2017-08-08 11:12:57 +1000

    Uploading Asteroid Game Project files

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// SHIFT + ALT + (UP or DOWN Arrows)
+
+public class Player : MonoBehaviour
+{
+    // <access-specifier>
+    public int lives = 3;
+    public float rotationSpeed = 2;
+    public float movementSpeed = 5;
+    public float acceleration = 50f;
+    public float deceleration = .1f;
+
+    private Rigidbody2D rigid;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Before Setting Rigid:" + rigid);
+        rigid = GetComponent<Rigidbody2D>();
+        Debug.Log("After Setting Rigid:" + rigid);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If user presses W
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            // Move forward
+            rigid.AddForce(transform.right * acceleration);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            rigid.AddForce(-transform.right * acceleration);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.rotation *= Quaternion.AngleAxis(rotationSpeed, Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.rotation *= Quaternion.AngleAxis(rotationSpeed, Vector3.back);
+        }
+        // Deceleration
+        rigid.velocity += -rigid.velocity * deceleration;
+    }
+}
---
+++ Assets/Scripts/ScreenWrap.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script depends on the SpriteRenderer component attached to the same GameObject
+[RequireComponent(typeof(SpriteRenderer))]
+public class ScreenWrap : MonoBehaviour
+{
+    // Sprite
+    private SpriteRenderer spriteRenderer;
+    // Camera 
+    private Bounds camBounds;
+    private float camWidth;
+    private float camHeight;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Camera boundary checker
+    void UpdateCameraBounds()
+    {
+        // Calculate camera bounds
+        Camera cam = Camera.main;
+        camHeight = 2f * cam.orthographicSize;
+        camWidth = camHeight * cam.aspect;
+        camBounds = new Bounds(cam.transform.position, new Vector2(camWidth, camHeight));
+    }
+
+    // Use LateUpdate since we are using the camera to wrap objects back around
+    void LateUpdate()
+    {
+        UpdateCameraBounds();
+        // store position and size in shorter variable names
+        Vector3 pos = transform.position; // position of our enemy
+        Vector3 size = spriteRenderer.bounds.size; // the size of our enemy
+        // calculate the sprites half width and height
+        float halfWidth = size.x / 2;
+        float halfHeight = size.y / 2;
+        float halfCamWidth = camWidth / 2f;
+        float halfCamHeight = camHeight / 2f;
+        // Check Left
+        if (pos.x + halfWidth < camBounds.min.x)
+        {
+            pos.x = camBounds.max.x + halfWidth;
+        }
+        // Check Right
+        if (pos.x - halfWidth > camBounds.max.x)
+        {
+            pos.x = camBounds.min.x - halfWidth;
+        }
+        // Check Top
+        if(pos.y - halfHeight > camBounds.max.y)
+        {
+            pos.y = camBounds.min.y - halfHeight;
+        }
+        // Check Bottom
+        if(pos.y + halfHeight < camBounds.min.y)
+        {
+            pos.y = camBounds.max.y + halfHeight;
+        }
+        // set new position
+        transform.position = pos;
+    }
+}
